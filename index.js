a947f9d05e79b85b05c00ef8a9c27316400be9d5
yanottamao <oktatama4@gmail.com>
2021-03-21 20:16:53 +0700

    Add click function

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 window.addEventListener("load", () => {
   const sounds = document.querySelectorAll(".sound");
   const pads = document.querySelectorAll(".pads div");
+
+  // Lets get going with the sound here
+  pads.forEach((pad) => {
+    pad.addEventListener("click", function () {
+      sounds[4];
+    });
+  });
 });
