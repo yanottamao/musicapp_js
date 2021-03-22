712a2ba4f014cd4fc04c8785beb35c91889d712a
yanottamao <oktatama4@gmail.com>
2021-03-22 20:20:05 +0700

    Add playing function

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ window.addEventListener("load", () => {
   const sounds = document.querySelectorAll(".sound");
   const pads = document.querySelectorAll(".pads div");
 
+  console.log(sounds);
+
   // Lets get going with the sound here
-  pads.forEach((pad) => {
+  pads.forEach((pad, index) => {
     pad.addEventListener("click", function () {
-      sounds[4];
+      sounds[index].play;
     });
   });
 });
