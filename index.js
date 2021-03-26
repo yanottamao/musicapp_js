b6f5eb0c97ff17f74fed03fbb6c85778f59ee9fd
yanottamao <oktatama4@gmail.com>
2021-03-26 17:28:28 +0700

    Add visual color placeholder

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 window.addEventListener("load", () => {
   const sounds = document.querySelectorAll(".sound");
   const pads = document.querySelectorAll(".pads div");
+  const visual = document.querySelector(".visual");
+  const color = [
+    "#16a085",
+    "#a05416",
+    "#a01624",
+    "#24a016",
+    "#1632a0",
+    "#9216a0"
+  ];
 
   console.log(sounds);
 
